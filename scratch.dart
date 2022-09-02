c6579d1b22888ecdf6831f1315718923b26aa3e0
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-09-02 16:39:19 +0300

    Creating an asynchronous method by the use of Future. Final.

--- scratch.dart
+++ scratch.dart
@@ -4,10 +4,10 @@ void main() {
   performTasks();
 }
 
-void performTasks()  {
+void performTasks()  async {
   task1();
-  task2();
-  task3();
+  String task2Result = await task2();
+  task3(task2Result);
 }
 
 void task1() {
@@ -15,19 +15,23 @@ void task1() {
   print('Task 1 complete');
 }
 
-void task2()  {
+Future<String> task2()  async{    //You may or not use <String> but the <datatype> indicate that we are expecting a Future input
   Duration fourSeconds = Duration(seconds: 4);
-  Future.delayed(fourSeconds, (){ //Creating an asynchronous method
-    String result = 'task 2 data';
+  String result;
+
+  await Future.delayed(fourSeconds, (){ //Creating an asynchronous method
+    result = 'task 2 data';
     print('Task 2 complete');
   });
+
+  return result;
 
   // sleep(fourSeconds);
   // String result = 'task 2 data';
   // print('Task 2 complete');
 }
 
-void task3() {
+void task3(String task2Data) {
   String result = 'task 3 data';
-  print('Task 3 complete');
+  print('Task 3 complete with $task2Data');
 }
