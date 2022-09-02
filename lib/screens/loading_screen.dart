8e1aa69412eed7ef5cc3252a8538b2c90a681cb5
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2022-09-02 15:54:39 +0300

    Initial upload of the Clima - Flutter App
    Initial upload. Activities done:
    1. Moved the app to version 2 embedding.
    2. Downloaded the geo locator plugin.
    3. Changed the application name retriever in the manifest.
    4. Imported the geo locator plugin in main.dart.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +8,10 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void _determinePosition() async{
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position); //Getting the location output
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            _determinePosition();
           },
           child: Text('Get Location'),
         ),
